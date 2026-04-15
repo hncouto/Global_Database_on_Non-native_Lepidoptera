e9e321e1cd30f1004db80b584c02aabd26519c38
hncouto <henriquenunocouto@gmail.com>
2026-04-15 11:39:46 +0100

    Data Cleaning Update
    Insertion of Extraction of Established and Introduced Species List before cleaning of native distribution data for clarity on the process.

--- Code/ObservationData.py
+++ Code/ObservationData.py
@@ -1,9 +1,10 @@
 # 1) Required Libraries
-import asyncio
-import nest_asyncio
-from tenacity import retry, wait_exponential, stop_after_attempt
-import aiohttp
-import pandas as pd
+
+import asyncio #used for the taxonomy updates loops to be done asynchronously and concurrently, saving up time on running the code.
+import nest_asyncio #used for the taxonomy updates loops to be done asynchronously and concurrently, saving up time on running the code.
+from tenacity import retry, wait_exponential, stop_after_attempt #used to guarantee that the extraction code was not stopped if an error occurred, and if it did occurred to run again for a predefined amount of times with a waiting time between them.
+import aiohttp #used to call the different API's, in this case the GBIF Species API, while working with *asyncio* and *nest_asyncio*.
+import pandas as pd #used to format the data on to the final schema and to work with the dataframes.
 
 # 2) Required Data
 
@@ -23,7 +24,7 @@ import pandas as pd
 # - ReferenceYear*: The year of the source reference, keeping in consideration that a species that was established at a certain point of time can now be eradicated.
 
 ObsList = pd.read_csv(r"../Data Raw/ObsList.csv", sep=";")
-NativeData = pd.read_csv(r"../Data Raw/NativeData.csv", sep=";") # List of species extracted during step 4.1 of this script. Native distribution data retrieved by hand between step 4.1 and 4.2.
+
 
 # 3) Taxonomy Standardization
 
@@ -124,7 +125,7 @@ async def VNF_Sessions_1(species_list):
 def VNF_Extractor_1(species_list):
     return asyncio.get_event_loop().run_until_complete(VNF_Sessions_1(species_list))
 
-# 4) Data Cleaning Observation Data
+# 4) Data Cleaning
 
 # Before doing the standardization we kept only the first two words on the *Species* field to keep only the "Genus + Specific Epithet", dropping any possible subspecies that had been recorded and any case that was not identified to the species level.
 # We also cleaned encoding and formatting issues (such as *¬†* characters) that could be present based on different file types.
@@ -149,17 +150,124 @@ ObservationsClean[ObservationsClean['AcceptedSpecies'].isna()]
 ObservationsClean.reset_index(drop=True, inplace=True)
 ObservationsClean.to_csv(r'../Transformed Data/RecordsClean.csv', sep =';', encoding='utf-8', index=False)
 
-# 4.1) Extraction Established and Introduced Species Distribution Data
+#5) Extraction Taxonomy and References
+
+# 5.1) Rquired Data
+# ObservationsClean = pd.read_csv(r'../Transformed Data/RecordsClean.csv', sep=';', encoding="utf-8")
+
+ObsData = ObservationsClean.copy()
+
+# 5.2) Functions
+
+# 5.2.1) GBIF Family Extractor
+nest_asyncio.apply()
+@retry(wait=wait_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(5)) #used to guarantee that the extraction code was not stopped if an error occurred, and if it did occurred to run again for a predefined amount of times with a waiting time between them.
+async def GBIF_Family_List(session, species):
+    '''
+    List Retainer: Extracts the family of a species from the GBIF Species API.
+    
+    Args:
+        session: aiohttp.ClientSession object. 
+        species: str. corresponding to the species name.
+        
+    Returns:
+        str.
+    '''
+    url = f"https://api.gbif.org/v1/species/match?name={species}" #GBIF Species API for the species listed
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            data = await response.json()
+            
+            if data.get('family'):
+                return data.get('family')
+            else:
+                return None
+    
+    except Exception as e:
+        print(f"Request failed for species '{species}': {e}")
+        return None
+
+async def GBIF_Family_Sessions(species_list):
+    '''
+    Session Creator: Takes a list of species and for each species in the list it creates a session to be called in the future.
+    
+    Args:
+        species_list: list. list of species names.
+        
+    Returns:
+        sessions list for each species listed.
+    '''
+    async with aiohttp.ClientSession() as session:
+        tasks = [GBIF_Family_List(session, species) for species in species_list]
+        return await asyncio.gather(*tasks)
+
+def GBIF_Family_Extract(species_list):
+    '''The Extractor: It calls the Session Creator, running the list of tasks returning the results for each genera in the list using the function on the List Retainer.
+    
+    Args:
+        species_list: list. list of species names.
+        
+    Returns:
+        Family list corresponding to the species listed.
+    '''
+    return asyncio.get_event_loop().run_until_complete(GBIF_Family_Sessions(species_list))
 
 
-# 4.2) Native Distribution Data
-# We repeated the standardization methodology for the native distribution data that was previously recorded for each species that was introduced and established.
+# 5.2.2) Lepidoptera Cross-Check
+nest_asyncio.apply()
+@retry(wait=wait_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(5)) #used to guarantee that the extraction code was not stopped if an error occurred, and if it did occurred to run again for a predefined amount of times with a waiting time between them.
+async def GBIF_Lepidoptera_List(session, family):
+    
+    url = f"https://api.gbif.org/v1/species/match?name={family}" #GBIF Species API for the family listed
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            data = await response.json()
+            
+            if data.get('order') == 'Lepidoptera':
+                return 1
+            elif data.get('order') is None:
+                return None
+            else:
+                return 0
+            
+    except Exception as e:
+        print(f"Error fetching data for {family}: {e}")
+        return False
 
-NativeData['Species'] = NativeData['Species'].apply(lambda x: ' '.join(x.split()[:2]))
-NativeData = NativeData[~NativeData['Species'].str.contains(r'\bsp\.$', case=False, na=False)]
-NativeData['AcceptedSpecies'] = GBIF_Extractor_2(VNF_Extractor_1(GBIF_Extractor_1(NativeData['Species'])))
-NativeData['AcceptedSpecies'] = NativeData['AcceptedSpecies'].apply(lambda x: ' '.join(x.split()[:2]) if isinstance(x, str) else x)
-NativeData[NativeData['AcceptedSpecies'].isna()]['Species'].unique()
-NativeDataClean = NativeData[NativeData['AcceptedSpecies'].isin(ObservationsClean['AcceptedSpecies'])].copy()
-NativeDataClean.reset_index(drop=True, inplace=True)
-NativeDataClean.to_csv(r'../Transformed Data/NativeDataClean.csv', sep =';', encoding='utf-8', index=False)
+async def GBIF_Lepidoptera_Sessions(family_list):
+    async with aiohttp.ClientSession() as session:
+        tasks = [GBIF_Lepidoptera_List(session, family) for family in family_list]
+        return await asyncio.gather(*tasks)
+
+def GBIF_Lepidoptera_Extract(family_list):
+    '''The Extractor: It calls the Session Creator, running the list of tasks returning the results for each family in the list using the function on the List Retainer.
+    
+    Given a list of families checks if they are listed as Lepidoptera or not under the GBIF Species API.
+    
+    Args:
+        family_list: list. list of families names.
+        
+    Returns:
+        1 or 0 depending if the family is listed as Lepidoptera or not for the GBIF Species API.
+    '''
+    return asyncio.get_event_loop().run_until_complete(GBIF_Lepidoptera_Sessions(family_list))
+
+
+# 5.3) Extract Taxonomy
+TaxonomyData = ObsData[['Species', 'AcceptedSpecies']].copy().drop_duplicates().reset_index(drop=True) # Keep a list of each unique pair of Species and AcceptedSpecies
+TaxonomyData = pd.concat([TaxonomyData, 
+                          pd.DataFrame({
+                              'Species': list(set(TaxonomyData['AcceptedSpecies']) - set(TaxonomyData['Species'])),
+                              'AcceptedSpecies': list(set(TaxonomyData['AcceptedSpecies']) - set(TaxonomyData['Species']))})], 
+                         ignore_index=True).drop_duplicates().reset_index(drop=True) # At the end of the dataframe for each AcceptedSpecies not in Species, add the AcceptedSpecies as Species and itself as AcceptedSpecies.
+
+# 5.3.1) Automatic Extraction Taxonomy
+
+TaxonomyData['Genus'] = TaxonomyData['AcceptedSpecies'].str.split(' ').str[0] #Keep only the Genus
+TaxonomyData['Family'] = GBIF_Family_Extract(TaxonomyData['AcceptedSpecies']) # Extract the Family
+TaxonomyData['Lepidoptera'] = GBIF_Lepidoptera_Extract(TaxonomyData['Family'])  # Check if the Family is Lepidoptera
+
+# 5.3.2) Manual Updates
+TaxonomyData[(TaxonomyData['Lepidoptera']==0)|(TaxonomyData['Lepidoptera'].isnull())] # Check which families were not identified as Lepidoptera
