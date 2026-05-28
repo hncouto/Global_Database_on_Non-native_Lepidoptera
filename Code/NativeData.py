0dc9e03f14453022228bf76c2a142febe313768d
hncouto <126601218+hncouto@users.noreply.github.com>
2026-05-28 11:47:52 +0100

    Native Data update
    Inclusion of data regarding native distribution of established species.

---
+++ Code/NativeData.py
@@ -0,0 +1,23 @@
+# 1) Required Libraries
+
+import asyncio
+import nest_asyncio
+from tenacity import retry, wait_exponential, stop_after_attempt
+import aiohttp
+import pandas as pd
+
+# 2) Required Data
+
+# All data was previously manually retrieved and fitted to a predefined schema.
+# For the Native data, here on after referred as *NativeData*, it corresponded to:
+# - Species: Species name as used on *Observation_TaxonomyClean*.
+# - Continent: The corresponding continent of origin of the species regarding the reference used.
+# - Realm: The biogeographic realm of origin fitting the updates defined at 10.1126/science.1228282.
+# - Reference Year: The year of the reference used to extract the data.
+# - Reference: The reference used to extract the data.
+
+NativeData = pd.read_csv(r'../Data Raw/NativeData.csv', sep=';', encoding="utf-8")
+
+# 3) Remove NA's
+
+NativeData[NativeData['Reference'].isna()]
